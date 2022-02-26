2d020370c6224e9415dcab04a001936d85876031
Praveen Dias <praveendias1180@gmail.com>
2022-02-26 23:56:23 +0530

    Using Arrow Functions

--- events.js
+++ events.js
@@ -2,8 +2,6 @@ const EventEmitter = require('events');
 
 const emitter = new EventEmitter();
 
-emitter.on('messageLogged', function(arg){
-    console.log('Listener called ' + arg);
-})
+emitter.on('messageLogged', (arg) => console.log('Listener called ' + arg));
 
 emitter.emit('messageLogged', { id:1, url: 'http://'});
